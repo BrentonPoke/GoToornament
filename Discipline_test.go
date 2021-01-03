124e3b427d3a4b2a09266558432225c145b2c66f
Brenton Poke <brentonpoke@outlook.com>
2021-01-02 19:00:55 -0500

    not sure if I want to try to triage enums this way, but it works

--- Discipline_test.go
+++ Discipline_test.go
@@ -11,7 +11,7 @@ func TestDiscipline(t *testing.T) {
 	var client ToornamentClient
 	client.ApiKey = os.Getenv("KEY")
 
-	disciplines := GetDisciplines(&client,NewDisciplineRange(0,7))
+	disciplines := GetDisciplines(&client,DisciplineScope().VIEWER,NewDisciplineRange(0,7))
 	str, err := json.Marshal(disciplines)
 	if err != nil {
 		t.Errorf("Couldn't find anything: %v",err)
@@ -26,7 +26,7 @@ func TestSingleDiscipline(t *testing.T) {
 	var client ToornamentClient
 	client.ApiKey = os.Getenv("KEY")
 
-	discipline := GetDiscipline(&client,"hearthstone")
+	discipline := GetDiscipline(&client,DisciplineScope().ORGANIZER,"hearthstone")
 	str, err := json.Marshal(discipline)
 	if err != nil {
 		t.Errorf("Couldn't find anything: %v",err)
--- Disciplines.go
+++ Disciplines.go
@@ -13,19 +13,29 @@ type DisciplineRange struct {
 	drange     string
 }
 
+type disciplineScope struct {
+	VIEWER string
+	PARTICIPANT string
+	ORGANIZER string
+}
+
+func DisciplineScope() *disciplineScope {
+	return &disciplineScope{VIEWER: "viewer", PARTICIPANT: "participant", ORGANIZER: "organizer"}
+}
+
 func NewDisciplineRange(begin, end int) *DisciplineRange {
 	d := DisciplineRange{begin: begin, end: end}
 	d.drange = "disciplines=" + strconv.Itoa(d.begin) + "-" + strconv.Itoa(d.end)
 	return &d
 }
 
-func GetDisciplines(c *ToornamentClient, disciplineRange *DisciplineRange) []Discipline {
+func GetDisciplines(c *ToornamentClient, apiScope string, disciplineRange *DisciplineRange) []Discipline {
 	client := resty.New()
 	resp, err := client.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("X-Api-Key", c.ApiKey).
 		SetHeader("range", disciplineRange.drange).
-		Get("https://api.toornament.com/viewer/v2/disciplines")
+		Get("https://api.toornament.com/"+apiScope+"/v2/disciplines")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,12 +48,12 @@ func GetDisciplines(c *ToornamentClient, disciplineRange *DisciplineRange) []Dis
 	return discipline
 }
 
-func GetDiscipline(c *ToornamentClient, id string) Discipline {
+func GetDiscipline(c *ToornamentClient, disciplineScope, id string) Discipline {
 	client := resty.New()
 	resp, err := client.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("X-Api-Key", c.ApiKey).
-		Get("https://api.toornament.com/viewer/v2/disciplines/"+id)
+		Get("https://api.toornament.com/"+disciplineScope+"/v2/disciplines/"+id)
 	if err != nil {
 		log.Fatal(err)
 	}
